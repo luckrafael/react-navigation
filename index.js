44aeb6160ab3def0df8f37e5e800b84c5314deaf
luckrafael <lrs.rafael@hotmail.com>
2022-02-15 11:31:12 -0300

    First commit using react navigation Stack

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
+import navegacao from './src/navegacao';
+
+AppRegistry.registerComponent(appName, () => navegacao);
---
+++ src/navegacao/Stack.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import TelaA from '../views/TelaA';
+import TelaB from '../views/TelaB';
+import TelaC from '../views/TelaC';
+
+const Stack = createNativeStackNavigator()
+
+export default props => (
+    <Stack.Navigator initialRouteName="TelaA" screenOptions={{ headerShown: true }} >
+        <Stack.Screen name="TelaA" options={{ title: 'Informações Iniciais' }} component={TelaA} />
+        <Stack.Screen name="TelaB" component={TelaB} />
+        <Stack.Screen name="TelaC" component={TelaC} />
+    </Stack.Navigator>
+)
